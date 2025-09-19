04f6eb6ab49dc4bc5d25ab74f544943a3ca390a4
artem-hedgehog <boiko2306@mail.ru>
2025-09-19 16:17:54 +0000

    Коммит

--- Lab_1/sandbox/Boiko.asm
+++ Lab_1/sandbox/Boiko.asm
@@ -6,35 +6,31 @@ msg2 db "Artem", 0xA, 0
 msg3 db "Andreevich", 0xA, 0
 
 _start:
-    ;инициализация регистров для вывода информации на экран
+
     mov eax, 4
     mov ebx, 1
     mov ecx, msg
     mov edx, 12
     int 0x80
 
-    ;инициализация регистров для вывода информации на экран
     mov eax, 4
     mov ebx, 1
     mov ecx, msg1
     mov edx, 7
     int 0x80
 
-    ;инициализация регистров для вывода информации на экран
     mov eax, 4
     mov ebx, 1
     mov ecx, msg2
     mov edx, 7
     int 0x80
 
-    ;инициализация регистров для вывода информации на экран
     mov eax, 4
     mov ebx, 1
     mov ecx, msg3
     mov edx, 12
     int 0x80
     
-    ;инициализация регистров для успешного завершения работы программы
     mov eax, 1
     mov ebx, 0
     int 0x80
